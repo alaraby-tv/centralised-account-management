25a84f461e572bb758aa0822fefa6d6aec314784
alaraby-tv <pmarkowski@alaraby.tv>
2020-04-07 17:02:46 +0100

    delete unnessecary files

--- db/migrate/20200305114755_create_join_table_access_acounts_permissions.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableAccessAcountsPermissions < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :access_accounts, :permissions do |t|
-      # t.index [:access_account_id, :permission_id]
-      # t.index [:permission_id, :access_account_id]
-    end
-  end
-end
