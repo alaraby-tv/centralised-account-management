8eae931d336fd3c6e459a0adbc26c3f74020428d
alaraby-tv <pmarkowski@alaraby.tv>
2020-04-08 09:37:17 +0100

    add end user reference

--- db/migrate/20200228134625_create_access_requests.rb
+++ db/migrate/20200228134625_create_access_requests.rb
@@ -3,6 +3,7 @@ class CreateAccessRequests < ActiveRecord::Migration[5.2]
     create_table :access_requests do |t|
       t.references :access_account, foreign_key: true
       t.references :request, foreign_key: true
+      t.references :end_user, foreign_key: true
       t.string :note
 
       t.timestamps
