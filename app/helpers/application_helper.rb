c77a0d924bd6a5ab7f48d5e9d098caba4808d76e
alaraby-tv <pmarkowski@alaraby.tv>
2020-04-14 09:59:29 +0100

    implement label_class method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def label_class(state)
+    case state
+    when 'submitted'
+      'primary'
+    when 'resubmitted'
+      'info'
+    when 'approved'
+      'warning'
+    when 'rejected'
+      'danger'
+    when 'completed'
+      'success'
+    else
+      'default'
+    end
+  end
 end
