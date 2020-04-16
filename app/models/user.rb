62831e4119b5fde634784eaa8016dec803cecaff
alaraby-tv <pmarkowski@alaraby.tv>
2020-04-16 23:00:39 +0100

    implement support member method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,18 @@ class User < ApplicationRecord
   has_many :access_accounts, foreign_key: "approver_id", inverse_of: :approver
   has_many :requests, foreign_key: "requester_id", inverse_of: :requester
   has_many :access_requests, through: :requests
+
+  delegate :name, to: :role, prefix: true
   
   def name
     "#{first_name} #{last_name}"
   end
 
   def admin?
-    role.name == 'admin'
+    role_name == 'admin'
+  end
+
+  def support_member?
+    role_name == 'broadcast engineer' or role_name == 'IT engineer'
   end
 end
