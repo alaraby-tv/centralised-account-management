06cba6f86f0522df0c64dbd47da77073f6e4d1a8
alaraby-tv <pmarkowski@alaraby.tv>
2020-04-19 19:41:52 +0100

    generate access request email and its views

---
+++ app/mailers/access_request_mailer.rb
@@ -0,0 +1,47 @@
+class AccessRequestMailer < ApplicationMailer
+
+  
+  def submit(access_request)
+    @access_request = access_request
+    @request = @access_request.request
+    @email = @access_request.approvable ? @access_request.approver.email : "support@alaraby.tv"
+    mail to: @email, subject: "Access Request Submitted"
+  end
+  
+  def resubmit(access_request)
+    @access_request = access_request
+    @request = @access_request.request
+    @email = @access_request.approvable ? @access_request.approver.email : "support@alaraby.tv"
+    mail to: @email, subject: "Access Request Resubmitted"
+  end
+  
+  def approve(access_request)
+    @access_request = access_request
+    @request = @access_request.request
+    mail to: "support@alaraby.tv", subject: "Access Request Approved"
+  end
+
+  def reject(access_request)
+    @access_request = access_request
+    @request = @access_request.request
+    mail to: @access_request.requester.email, subject: "Access Request Rejected"
+  end
+
+  def cancel(access_request)
+    @access_request = access_request
+
+    mail to: "support@alaraby.tv", subject: "Access Request Cancelled"
+  end
+
+  def complete(access_request)
+    @access_request = access_request
+    @request = @access_request.request
+    mail to: @access_request.requester.email, subject: "Access Request Granted"
+  end
+
+  # Subject optionally can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #  e.g.: en.access_request_mailer.complete.subject
+  #
+end
