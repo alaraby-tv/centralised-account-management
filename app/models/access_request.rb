db7ebcbd867d38a652b59640a8a5e12ca2e50615
alaraby-tv <pmarkowski@alaraby.tv>
2020-04-19 19:21:34 +0100

    implement redy to complete method

--- app/models/access_request.rb
+++ app/models/access_request.rb
@@ -12,9 +12,8 @@ class AccessRequest < ApplicationRecord
   delegate :name, to: :access_account, prefix: true
   delegate :name, to: :end_user, prefix: true
   delegate :name, to: :permission
-  delegate :approver, to: :access_account
-  delegate :requester, to: :request
-  delegate :requester_name, to: :request
+  delegate :approvable, :approver, :approver_name, :approver_email, to: :access_account
+  delegate :requester, :requester_name, :requester_email, to: :request
 
   STATES = %w[draft submitted resubmitted approved rejected completed cancelled]
   delegate :draft?, :submitted?, :resubmitted?, :approved?, :rejected?, :completed?, :cancelled?, to: :current_state
@@ -28,33 +27,45 @@ class AccessRequest < ApplicationRecord
   end
 
   def submit(user)
-    @event = access_request_events.create!(state: "submitted", user_name: user.name) if current_state.draft?
-    # RequestStatusMailer.new_request_notification(self).deliver
+    if current_state.draft?
+      @event = access_request_events.create!(state: "submitted", user_name: user.name)
+      AccessRequestMailer.submit(self).deliver
+    end
   end
 
   def resubmit(user)
-    access_request_events.create!(state: "resubmitted", user_name: user.name) if ready_to_resubmit?
-    # RequestStatusMailer.updated_request_notification(self).deliver
+    if ready_to_resubmit?
+      access_request_events.create!(state: "resubmitted", user_name: user.name)
+      AccessRequestMailer.resubmit(self).deliver
+    end
   end
 
   def approve(user, comment)
-    access_request_events.create!(state: "approved", user_name: user.name, comment: comment) if ready_to_approve?
-    # RequestStatusMailer.approved_request_notification(self).deliver
+    if ready_to_approve?
+      access_request_events.create!(state: "approved", user_name: user.name, comment: comment)
+      AccessRequestMailer.approve(self).deliver
+    end
   end
 
   def reject(user, comment)
-    access_request_events.create!( state: "rejected", user_name: user.name, comment: comment) if ready_to_approve?
-    # RequestStatusMailer.rejected_request_notification(self).deliver
+    if ready_to_approve?
+      access_request_events.create!(state: "rejected", user_name: user.name, comment: comment)
+      AccessRequestMailer.reject(self).deliver
+    end
   end
 
   def cancel(user, comment)
-    access_request_events.create!(state: "cancelled", user_name: user.name, comment: comment) if ready_to_cancel?
-    # RequestStatusMailer.completed_request_notification(self).deliver
+    if ready_to_cancel?
+      access_request_events.create!(state: "cancelled", user_name: user.name, comment: comment) 
+      AccessRequestMailer.cancel(self).deliver
+    end
   end
 
   def complete(user)
-    access_request_events.create!(state: "granted", user_name: user.name) if current_state.approved?
-    # RequestStatusMailer.completed_request_notification(self).deliver
+    if ready_to_complete?
+      access_request_events.create!(state: "granted", user_name: user.name) 
+      AccessRequestMailer.complete(self).deliver
+    end
   end
 
   def access_account_with_permissions?
@@ -63,6 +74,10 @@ class AccessRequest < ApplicationRecord
 
   def ready_to_approve?
     current_state.submitted? or current_state.resubmitted?
+  end
+
+  def ready_to_complete?
+    approvable ? current_state.approved? : ready_to_approve?
   end
 
   def ready_to_resubmit?
