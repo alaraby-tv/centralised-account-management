910d723f852454a9b5420ede27116db8ca0ebb2e
alaraby-tv <pmarkowski@alaraby.tv>
2020-04-20 18:08:36 +0100

    set request as instance variable

--- app/mailers/access_request_mailer.rb
+++ app/mailers/access_request_mailer.rb
@@ -3,39 +3,39 @@ class AccessRequestMailer < ApplicationMailer
   
   def submit(access_request)
     @access_request = access_request
-    
+    @request = @access_request.request
     email = @access_request.approvable ? @access_request.approver.email : "support@alaraby.tv"
     mail to: email, subject: "Access Request Submitted"
   end
   
   def resubmit(access_request)
     @access_request = access_request
-    
+    @request = @access_request.request
     email = @access_request.approvable ? @access_request.approver.email : "support@alaraby.tv"
     mail to: email, subject: "Access Request Resubmitted"
   end
   
   def approve(access_request)
     @access_request = access_request
-    
+    @request = @access_request.request
     mail to: "support@alaraby.tv", subject: "Access Request Approved"
   end
 
   def reject(access_request)
     @access_request = access_request
-    
+    @request = @access_request.request
     mail to: @access_request.access_requester_email, subject: "Access Request Rejected"
   end
 
   def cancel(access_request)
     @access_request = access_request
-
+    @request = @access_request.request
     mail to: "support@alaraby.tv", subject: "Access Request Cancelled"
   end
 
   def complete(access_request)
     @access_request = access_request
-    
+    @request = @access_request.request
     mail to: @access_request.access_requester_email, subject: "Access Request Granted"
   end
 
