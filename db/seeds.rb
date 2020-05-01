39e529ba69c46710765cb7b489c50ea72ac8780c
alaraby-tv <pmarkowski@alaraby.tv>
2020-05-01 18:48:13 +0100

    sort the glyphicon issue in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-admin = Role.create!(name: "admin")
+superuser = Role.create!(name: "superuser")
 User.create!(first_name: "Przemyslaw",
              last_name: "Markowski",
-             role: admin,
+             admin: true,
+             role: superuser,
              email: "pmarkowski@alaraby.tv",
              password: "Changeme1",
              password_confirmation: "Changeme1")
